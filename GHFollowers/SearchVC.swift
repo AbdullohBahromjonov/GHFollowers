1a243a609c3a54228fcd7c53889a77aba0aabbbf
Abdulloh <abdullohbahromjonv@gmail.com>
2024-01-29 15:33:59 +0500

    Created SearchVC and FavoritesVC. Created Navigation Controller base

---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Abdulloh on 28/01/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
